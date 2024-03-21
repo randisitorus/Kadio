6a82b86ea980691c19860eb412173312b3846905
randisitorus <randiasitorus@yahoo.com>
2024-03-21 15:51:12 +0000

    🔴 auto commit

--- Update.tsx
+++ Update.tsx
@@ -2,3 +2,4 @@
 project id required in the url
 project id required in the url
 project id required in the url
+{"error":"{\"Error_GUID\":\"9347086996083549494\",\"Error\":\"Failed all retries {GUID:9347086996083549494,error:No pairings available.,chain_id:ETH1}\"}"}

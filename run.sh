be0fdc95ffcd5826e80bd7a5acae088ea29f53f5
Randi Sitorus <59337872+randisitorus@users.noreply.github.com>
2024-03-21 22:51:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://mainnet.infura.io/v3/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c31f0c0a-d905-4aa0-83a8-49846d41cbe8/)
 
 
 echo "$curl_output" >> Update.tsx
